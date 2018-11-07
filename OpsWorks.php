4844791b34a8eb74b9ab99ab6717e25cbb318440
IPSpecialist <43603376+IPSpecialist@users.noreply.github.com>
2018-11-07 11:20:11 +0500

    Create OpsWorks.php

---
+++ OpsWorks.php
@@ -0,0 +1,18 @@
+<? 
+} 
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <title>IPSpecialist</title>
+</head>
+<body>
+  <h1 align="center">Welcome to IPSpecialist</h1><br>
+  <div align="center"><a img src="https://ipspecialist.net/wp-content/uploads/2018/07/IPS-New-Logo-1-2-e1531096497517.png"></a></div>
+  <p>Your Application is now running on the host "<?php echo gethostname(); ?>"; in a new stack and layer in OpsWorks.</p>
+                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+</body>
+</html>  
+<? 
+} 
+?>
